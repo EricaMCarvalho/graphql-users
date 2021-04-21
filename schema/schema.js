372ebb83d9c46ee541bdbf7f8edc7f506946ebd7
Erica Carvalho <ericamcar28@gmail.com>
2021-04-21 17:56:27 +0100

    delete and edit

--- schema/schema.js
+++ schema/schema.js
@@ -1,14 +1,49 @@
 const graphql = require('graphql');
 const axios = require('axios');
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull,
+} = graphql;
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  // we need an arrow function because when the file first runs, it gets defined but it doesn't run right away
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parent, args, ctx, info) {
+        return axios
+          .get(`http://localhost:3000/companies/${parent.id}/users`)
+          .then((res) => res.data)
+          .catch((err) => console.log(err));
+      },
+    },
+  }),
+});
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
-  },
+    company: {
+      type: CompanyType,
+      resolve(parent, args, ctx, info) {
+        return axios
+          .get(`http://localhost:3000/companies/${parent.companyId}`)
+          .then((res) => res.data)
+          .catch((err) => console.log(err));
+      },
+    },
+  }),
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -25,9 +60,66 @@ const RootQuery = new GraphQLObjectType({
           .catch((err) => console.log(err));
       },
     },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args, ctx, info) {
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
+          .then((res) => res.data)
+          .catch((err) => console.log(err));
+      },
+    },
+  },
+});
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+        companyId: { type: GraphQLString },
+      },
+      resolve(parent, { firstName, age }, ctx, info) {
+        return axios
+          .post('http://localhost:3000/users', {
+            firstName,
+            age,
+          })
+          .then((res) => res.data);
+      },
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, { id }) {
+        return axios
+          .delete(`http://localhost:3000/users/${id}`)
+          .then((res) => res.data);
+      },
+    },
+    updateUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+      },
+      resolve(parent, args) {
+        return axios
+          .patch(`http://localhost:3000/users/${id}`, args)
+          .then((res) => res.data);
+      },
+    },
   },
 });
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation,
 });
